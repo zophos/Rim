22ee33268ff26355803c00e4b27968fb0e38d477
unknown <zophos@.(none)>
2010-04-06 09:08:18 +0900

    * src/ar.rb : separate marker finding algorithm * src/ar-marker-*.rb : various marker finding algorithms

---
+++ src/ar-marker-loose.rb
@@ -0,0 +1,223 @@
+#
+# ar-marker-loose.rb
+#
+class Edge
+    def initialize(id,p1,p2)
+        @id=id
+        @p1=p1
+        @p2=p2
+
+        @vec=p2-p1
+        @vec.div!(NMath.sqrt(@vec**2))
+    end
+    attr_reader :id,:p1,:p2,:vec
+
+    def distance(other)
+        1.0-self.cos(other)
+        #_heron(@p1,@p2,other.p2)+_heron(@p1,other.p1,other.p2)
+
+        #l1=(@p1-other.p1)**2
+	#l2=(@p2-other.p2)**2
+
+        #[l1,l2].sort.last
+    end
+
+    def _heron(p1,p2,p3)
+        a=p2-p1
+        b=p3-p2
+        c=p1-p3
+
+        a=NMath.sqrt(a**2)
+        b=NMath.sqrt(b**2)
+        c=NMath.sqrt(c**2)
+
+        s=(a+b+c)/2.0
+
+        NMath.sqrt(s*(s-a)*(s-b)*(s-c))
+    end
+
+    def cos(other)
+        @vec*other.vec
+    end
+end
+
+class Marker
+    @@seq=0
+
+    #
+    # marker grid points
+    #
+    WORLD_POINTS=NVector.to_na([[-30.0,30.0,0.0],
+                                [30.0,30.0,0.0],
+                                [-30.0,-30.0,0.0],
+                                [30.0,-30.0,0.0]])
+    def self.remap(m1,m2)
+        r1=m1.base_edge_candidates(m2)
+        r2=m2.base_edge_candidates(m1)
+        
+        min_pair=nil
+        min_dist=nil
+        
+        r1.each{|e|
+            r2.each{|f|
+                d=e.distance(f)
+                
+                if(!min_dist || min_dist>d)
+                    min_dist=d
+                    min_pair=[e,f]
+                end
+            }
+        }
+
+        m1.remap(min_pair[0])
+        m2.remap(min_pair[1])
+    end
+
+    def initialize(id,vertex,gray,bin,fill,t1,t2,warp)
+        @seq=@@seq
+        @@seq+=1
+
+        @id=id
+        @rot=0
+        @is_xy=nil
+        @is_xz=nil
+
+        _mapping(vertex)
+    end
+    attr_reader :id,:world,:camera,:cog,:is_xy,:is_xz,:rot
+
+    def size
+        if(@world.dim>1)
+            @world.shape[1]
+        else
+            0
+        end
+    end
+
+    def edge(n)
+        case n
+        when 0
+            Edge.new(n,@camera[true,0],@camera[true,1])
+        when 1
+            Edge.new(n,@camera[true,1],@camera[true,3])
+        when 2
+            Edge.new(n,@camera[true,2],@camera[true,3])
+        when 3
+            Edge.new(n,@camera[true,0],@camera[true,2])
+        else
+            nil
+        end
+    end
+
+    def base_edge_candidates(other)
+        if(self.cog[1]<other.cog[1])
+            @is_xy=1
+            @is_xz=0
+
+            if(self.cog[0]<other.cog[0])
+                [self.edge(2),self.edge(1)]
+            else
+                [self.edge(2),self.edge(3)]
+            end
+        else
+            @is_xy=0
+            @is_xz=1
+
+            if(self.cog[0]<other.cog[0])
+                [self.edge(0),self.edge(1)]
+            else
+                [self.edge(0),self.edge(3)]
+            end
+        end
+    end
+
+
+    def is_xy?
+        if(@is_xy&&@is_xz)
+            @is_xy>@is_xz
+        else
+            @seq%2==0
+        end
+    end
+
+    def is_xz?
+        !self.is_xy?
+    end
+
+    def probability
+        1.0
+    end
+
+    def remap(e)
+        if(@is_xy>@is_xz)
+            _remap_xy(e.id)
+        else
+            _remap_xz(e.id)
+        end
+
+        self
+    end
+
+    private
+    def _mapping(vertex)
+        @cog=NVector.to_na(vertex).mean(1)
+
+        #
+        # sort rectangle vertex order lt->rt->lb->rb
+        #
+        vertex.sort!{|a,b|
+            if((a[1]-cog[1])*(b[1]-cog[1])<0)
+                a[1]<=>b[1]
+            else
+                a[0]<=>b[0]
+            end
+        }
+
+        #
+        # left-top is origin
+        #
+        @origin=vertex[0]
+
+        @residual=nil
+        @camera=NVector.to_na(vertex)
+        @world=WORLD_POINTS[]
+    end
+
+
+    def _remap_xy(e)
+        case e
+        when 0
+            @world[0..1,true]=ROT180*@world[0..1,true]
+            @rot=180
+        when 1
+            @world[0..1,true]=ROT90*@world[0..1,true]
+            @rot=90
+        when 2
+            @rot=0
+        when 3
+            @world[0..1,true]=ROT270*@world[0..1,true]
+            @rot=270
+        end
+
+        @world[2,true]=XY_PALNE_Z
+    end
+
+    def _remap_xz(e)
+        case e
+        when 0
+            @rot=0
+        when 1
+            @world[0..1,true]=ROT270*@world[0..1,true]
+            @rot=270
+        when 2
+            @world[0..1,true]=ROT180*@world[0..1,true]
+            @rot=180
+        when 3
+            @world[0..1,true]=ROT90*@world[0..1,true]
+            @rot=90
+        end
+
+        @world[2,true]=@world[1,true].mul!(-1.0)
+        @world[1,true]=XZ_PLANE_Y
+    end
+end
---
+++ src/ar-marker-strict.rb
@@ -0,0 +1,243 @@
+#
+# ar-marker-strict.rb
+#
+class Marker
+    def self.remap(m1,m2)
+        m1.remap(m2)
+        m2.remap(m1)
+    end
+
+    def initialize(id,vertex,gray,bin,fill,t1,t2,warp)
+        @id=id
+        @rot=0
+
+        _get_perspective_matrix(vertex)
+        _mapping(gray,bin,fill,t1,t2)
+        _sampling(bin,warp)
+        _count_sample
+    end
+    attr_reader :id,:world,:camera,:cog,:is_xy,:is_xz,:rot
+
+    def size
+        if(@world.dim>1)
+            @world.shape[1]
+        else
+            0
+        end
+    end
+
+    def is_xy?
+        @is_xy>@is_xz
+    end
+
+    def is_xz?
+        !self.is_xy?
+    end
+    
+    def probability
+        if(@is_xy+@is_xz==0)
+            -1.0
+        else
+            (@is_xy-@is_xz).to_f.abs/(@is_xy+@is_xz).to_f
+        end
+    end
+
+    def remap(o)
+        if(self.is_xy?)
+            _remap_xy(o)
+        else
+            _remap_xz(o)
+        end
+        self
+    end
+
+    private
+    def _get_perspective_matrix(vertex)
+        @cog=NVector.to_na(vertex).mean(1)
+
+        #
+        # sort rectangle vertex order lt->rt->lb->rb
+        #
+        vertex.sort!{|a,b|
+            if((a[1]-cog[1])*(b[1]-cog[1])<0)
+                a[1]<=>b[1]
+            else
+                a[0]<=>b[0]
+            end
+        }
+
+        #
+        # left-top is origin
+        #
+        @origin=vertex[0]
+
+        #
+        # calc destination squire's edge length
+        #
+        len=NMath.sqrt([(vertex[1]-vertex[0])**2,
+                        (vertex[2]-vertex[0])**2,
+                        (vertex[3]-vertex[1])**2,
+                        (vertex[3]-vertex[2])**2,
+                       ].sort.first)
+        
+        @magnify=60.0/len
+        
+        #
+        # generate distination coord vector
+        #
+        warp_coord=NVector.float(2,4)
+
+        warp_coord[0,true]=vertex[0][0]
+        warp_coord[1,true]=vertex[0][1]
+
+        warp_coord[0,1]+=len
+        warp_coord[1,2]+=len
+        warp_coord[true,3]=warp_coord[true,3].add!(len)
+        
+        #
+        # solv parspective transform matrix
+        #
+        src_mat=NMatrix.float(8,8)
+        4.times{|i|
+            j=i*2
+            src_mat[true,j..j+1]=
+                NMatrix.to_na([[vertex[i][0],vertex[i][1],1.0,
+                                      0.0,0.0,0.0,
+                                      -vertex[i][0]*warp_coord[0,i],
+                                      -vertex[i][1]*warp_coord[0,i]],
+                                  [0.0,0.0,0.0,
+                                      vertex[i][0],vertex[i][1],1.0,
+                                      -vertex[i][0]*warp_coord[1,i],
+                                      -vertex[i][1]*warp_coord[1,i]]])
+        }
+        r_vec=warp_coord.flatten/src_mat
+
+        @rot3x3=NMatrix.float(3,3)
+        @rot3x3[0..2,0]=r_vec[0..2]
+        @rot3x3[0..2,1]=r_vec[3..5]
+        @rot3x3[0..1,2]=r_vec[6..7]
+        @rot3x3[2,2]=1.0
+    end
+
+    def _mapping(gray,bin,fill,t1,t2)
+        #
+        # get characteristic courner list
+        #
+        bin.fill_hole(fill)
+        camera=gray.cv_find_courner_harris(t1,t2,fill)
+
+        raise 'Not enough marker points found.' if camera.size<3
+        
+        #
+        # mapping to world coordinate
+        #
+        vec=NVector.float(3)
+        world=camera.map{|v|
+            vec[0..1]=v
+            vec[2]=1.0
+            
+            vec=@rot3x3*vec
+            vec.div!(vec[2])
+            
+            t=vec[0..1].sbt!(@origin).mul!(@magnify).sbt!(30.0)
+
+            gp_i=t.round.div!(5).mul!(5)
+            gp=gp_i.to_f
+            if(((gp_i%10).eq(0).any?) ||
+               (gp[0].abs>25.0) ||
+               (gp[1].abs>25.0) ||
+               ((gp-t)**2)>9)
+                nil
+            else
+                NVector.to_na([gp[0],-gp[1],0.0])
+            end
+        }.each_with_index{|w,i|
+            camera[i]=nil unless w
+        }
+
+        camera.compact!
+        world.compact!
+
+        raise 'Not enough marker points found.' if camera.size<3
+
+        @residual=nil
+        @camera=NVector.to_na(camera)
+        @world=NVector.to_na(world)
+    end
+
+    def _sampling(img,work)
+        img.cv_warp_perspective(@rot3x3,work)
+
+        sampling_points=SAMPLING_POINTS[].add!(30.0).div!(@magnify)
+        sampling_points.add!(@origin)
+
+        @sample=NMatrix.byte(5,5)
+        i=0
+        5.times{|y|
+            5.times{|x|
+                pt=sampling_points[true,i].round
+                rx=(pt[0]-1)..(pt[0]+1)
+                ry=(pt[1]-1)..(pt[1]+1)
+                begin
+                    @sample[x,y]=1 if work[0,rx,ry].eq(0).sum>4
+                rescue
+                end
+                i+=1
+            }
+        }
+    end
+
+    def _count_sample
+        @is_xy=@sample[0,0]+@sample[4,0]+
+            @sample[1,1]+@sample[3,1]+
+            @sample[1,3]+@sample[3,3]+
+            @sample[0,4]+@sample[4,4]
+
+        @is_xz=@sample[1,0]+@sample[3,0]+
+            @sample[0,1]+@sample[4,1]+
+            @sample[0,3]+@sample[4,3]+
+            @sample[1,4]+@sample[3,4]
+    end
+
+    def _remap_xy(other)
+        n=@sample[2,0]+@sample[2,4]+2-(@sample[0,2]+@sample[4,2])
+
+        if(n>1)
+            if(self.cog[1]>other.cog[1])
+                @world[0..1,true]=ROT180*@world[0..1,true]
+                @rot=180
+            end
+        else
+            if(self.cog[0]<other.cog[0])
+                @world[0..1,true]=ROT90*@world[0..1,true]
+                @rot=90
+            else
+                @world[0..1,true]=ROT270*@world[0..1,true]
+                @rot=270
+            end
+        end
+        @world[2,true]=XY_PALNE_Z
+    end
+
+    def _remap_xz(other)
+        n=@sample[1,2]+@sample[3,2]+2-(@sample[2,1]+@sample[2,3])
+
+        if(n>1)
+            if(self.cog[1]<other.cog[1])
+                @world[0..1,true]=ROT180*@world[0..1,true]
+                @rot=180
+            end
+        else
+            if(self.cog[0]>other.cog[0])
+                @world[0..1,true]=ROT90*@world[0..1,true]
+                @rot=90
+            else
+                @world[0..1,true]=ROT270*@world[0..1,true]
+                @rot=270
+            end
+        end
+
+        @world[2,true]=@world[1,true].mul!(-1.0)
+        @world[1,true]=XZ_PLANE_Y
+    end
+end
